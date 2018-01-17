1c2fe59f8dcf4e5726ce14b4798f3e3286dd5909
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2018-01-17 18:46:09 +0800

    add unit test entities/Auth/User/RequestSignupTest.php

---
+++ shop/tests/unit/entities/Auth/User/RequestSignupTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo
+ * Date: 17.01.18
+ * Time: 17:08
+ */
+
+namespace Shop\tests\unit\entities\Auth\User;
+
+
+use Codeception\Test\Unit;
+use shop\entities\Auth\User;
+
+class RequestSignupTest extends Unit
+{
+    public function testSuccess()
+    {
+        $model = User::requestSignup(
+            $password = 'password',
+            $repeatPassword = 'password',
+            $login = 'login',
+            $email = 'email@test.com');
+        $this->assertEquals($login, $model->login, 'Login does not equals');
+        $this->assertEquals($email, $model->email, 'Email does not equals');
+        $this->assertEquals(User::STATUS_CONFIRM_EMAIL, $model->status, 'Status does not equals');
+        $this->assertNotEmpty($model->request_email_token, 'request_email_token is empty');
+        $this->assertAttributeInternalType('string', 'request_email_token', $model, 'request_email_token is not string');
+
+    }
+}
