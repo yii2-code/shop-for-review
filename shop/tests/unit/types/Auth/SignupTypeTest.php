73e83c79d54df95a2d4b5eaa237c73abd8e2a57a
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2018-01-18 20:47:17 +0800

    add SignupType

--- shop/tests/unit/types/Auth/SignupTypeTest.php
+++ shop/tests/unit/types/Auth/SignupTypeTest.php
@@ -9,41 +9,74 @@
 namespace shop\tests\unit\types\Auth;
 
 use Codeception\Test\Unit;
-use shop\types\User\SignupType;
+use shop\entities\Auth\User;
+use shop\tests\fixtures\UserFixture;
+use shop\tests\UnitTester;
+use shop\types\Auth\SignupType;
 
 class SignupTypeTest extends Unit
 {
-    /*    public function testSuccess()
-        {
-            $type = new SignupType();
-            $type->login = 'login';
-            $type->email = 'test@email.com';
-            $type->password = 'password';
-            $this->assertTrue($type->validate());
-        }
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
 
-        public function testRequired()
-        {
-            $type = new SignupType();
-            $this->assertFalse($type->validate());
-            $this->assertArrayHasKey('login', $type->getErrors());
-            $this->assertArrayHasKey('email', $type->getErrors());
-            $this->assertArrayHasKey('password', $type->getErrors());
-        }
+    public function testSuccess()
+    {
+        $type = new SignupType();
+        $type->login = 'login';
+        $type->email = 'test@email.com';
+        $type->password = 'password';
+        $type->repeatPassword = 'password';
+        $this->assertTrue($type->validate());
+    }
 
-        public function testEmail()
-        {
-            $type = new SignupType();
-            $type->email = 'tests';
-            $this->assertFalse($type->validate());
-            $this->assertArrayHasKey('email', $type->getErrors());
-        }
+    public function testRequired()
+    {
+        $type = new SignupType();
+        $this->assertFalse($type->validate());
+        $this->assertArrayHasKey('login', $type->getErrors());
+        $this->assertArrayHasKey('email', $type->getErrors());
+        $this->assertArrayHasKey('password', $type->getErrors());
+    }
 
-        public function testPassword()
-        {
-            $type = new SignupType();
-            $type->password = 'tst';
-            $this->assertFalse($type->validate());
-            $this->assertArrayHasKey('password', $type->getErrors());
-        }*/
+    public function testEmail()
+    {
+        $type = new SignupType();
+        $type->email = 'tests';
+        $this->assertFalse($type->validate());
+        $this->assertArrayHasKey('email', $type->getErrors());
+    }
+
+    public function testGreat()
+    {
+        $type = new SignupType();
+        $type->password = 'tst';
+        $type->login = 'ts';
+        $this->assertFalse($type->validate());
+        $this->assertArrayHasKey('password', $type->getErrors());
+        $this->assertArrayHasKey('login', $type->getErrors());
+    }
+
+    /**
+     * @throws \shop\tests\_generated\ModuleException
+     */
+    public function testUnique()
+    {
+        $this->tester->haveFixtures([
+            'user' => [
+                'class' => UserFixture::class,
+                'dataFile' => '@shop/tests/_data/user.php'
+            ]
+        ]);
+
+        /** @var User $user */
+        $user = $this->tester->grabFixture('user', 1);
+
+        $type = new SignupType();
+        $type->login = $user->login;
+        $type->email = $user->email;
+        $this->assertFalse($type->validate());
+        $this->assertArrayHasKey('password', $type->getErrors());
+    }
 }
---
+++ shop/types/Auth/SignupType.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo
+ * Date: 18.01.18
+ * Time: 20:29
+ */
+
+namespace shop\types\Auth;
+
+
+use shop\entities\Auth\User;
+use yii\base\Model;
+
+/**
+ * Class SignupType
+ * @package shop\types\Auth
+ */
+class SignupType extends Model
+{
+    /**
+     * @var
+     */
+    public $login;
+    /**
+     * @var
+     */
+    public $email;
+    /**
+     * @var
+     */
+    public $password;
+    /**
+     * @var
+     */
+    public $repeatPassword;
+
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'email', 'password', 'repeatPassword'], 'trim'],
+            [['login', 'email', 'password', 'repeatPassword'], 'required'],
+            [['email', 'repeatPassword'], 'string'],
+            ['password', 'string', 'min' => '6'],
+            ['login', 'string', 'min' => '3'],
+            ['email', 'email'],
+            ['email', 'unique', 'targetClass' => User::class],
+            ['login', 'unique', 'targetClass' => User::class],
+        ];
+    }
+}
