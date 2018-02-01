c416ba62292cbc687a49e9f4c499db47e13728c6
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2018-02-01 19:32:05 +0800

    add type CharacteristicType

---
+++ shop/tests/unit/types/Product/CharacteristicTypeTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo
+ * Date: 01.02.18
+ * Time: 19:08
+ */
+
+namespace shop\tests\unit\types\Product;
+
+
+use Codeception\Test\Unit;
+use shop\entities\Product\Characteristic;
+use shop\types\Product\CharacteristicType;
+
+/**
+ * Class CharacteristicTypeTest
+ * @package shop\tests\unit\types\Product
+ */
+class CharacteristicTypeTest extends Unit
+{
+    /**
+     * @group product
+     */
+    public function testSuccess()
+    {
+        $type = new CharacteristicType();
+        $type->title = 'title';
+        $type->type = Characteristic::TYPE_STRING;
+        $type->required = Characteristic::REQUIRED_YES;
+        $type->default = '200';
+        $type->variants = [300, 400];
+
+        $this->assertTrue($type->validate(), 'Unable to validate type');
+    }
+
+    /**
+     * @group product
+     */
+    public function testRequired()
+    {
+        $type = new CharacteristicType();
+        $this->assertFalse($type->validate(), 'Unable to validate type');
+        $this->assertArrayHasKey('title', $type->getErrors(), 'Attribute has not "title" error');
+        $this->assertArrayHasKey('type', $type->getErrors(), 'Attribute has not "type" error');
+        $this->assertArrayHasKey('required', $type->getErrors(), 'Attribute has not "required" error');
+    }
+}
---
+++ shop/types/Product/CharacteristicType.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo
+ * Date: 01.02.18
+ * Time: 18:55
+ */
+
+namespace shop\types\Product;
+
+
+use shop\entities\Product\Characteristic;
+use yii\base\Model;
+
+/**
+ * Class CharacteristicType
+ * @package shop\types\Product
+ */
+class CharacteristicType extends Model
+{
+    /**
+     * @var
+     */
+    public $title;
+
+    /**
+     * @var
+     */
+    public $type;
+
+    /**
+     * @var
+     */
+    public $required;
+
+    /**
+     * @var
+     */
+    public $default;
+
+    /**
+     * @var
+     */
+    public $variants;
+
+    /**
+     * @var Characteristic
+     */
+    public $model;
+
+
+    /**
+     * CharacteristicType constructor.
+     * @param Characteristic|null $model
+     * @param array $config
+     */
+    public function __construct(
+        Characteristic $model = null,
+        array $config = []
+    )
+    {
+        parent::__construct($config);
+        if (is_null($model)) {
+            $this->model = new Characteristic();
+        } else {
+            $this->title = $model->title;
+            $this->type = $model->type;
+            $this->required = $model->required;
+            $this->default = $model->default;
+            $this->variants = $model->variants;
+            $this->model = $model;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            ['title', 'trim'],
+            [['title', 'type', 'required'], 'required'],
+            [['default'], 'string'],
+            [['title'], 'string', 'max' => 100],
+            [['type', 'required'], 'integer'],
+            ['variants', 'safe']
+        ];
+    }
+}
