54f52265955ba510a375efe3335211e4eb4f484e
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2018-02-09 15:23:40 +0800

    change config console

--- console/config/main.php
+++ console/config/main.php
@@ -2,15 +2,17 @@
 $params = array_merge(
     require __DIR__ . '/../../common/config/params.php',
     require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+    require __DIR__ . '/params.php'
 );
 
 return [
     'id' => 'app-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log', 'gii'],
     'controllerNamespace' => 'console\controllers',
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+    ],
     'controllerMap' => [
         'migrate' => [
             'class' => \yii\console\controllers\MigrateController::class,
